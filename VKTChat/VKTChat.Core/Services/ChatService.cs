01532a8a240e6005617a8ce9bf545f611f9b5a57
Vktor <vkt.knnnk@gmail.com>
2024-03-26 22:57:33 +0500

    realized chat service

--- VKTChat/VKTChat.Core/Services/ChatService.cs
+++ VKTChat/VKTChat.Core/Services/ChatService.cs
@@ -4,35 +4,59 @@ namespace VKTChat.Core.Services
 {
     public class ChatService : IChatService
     {
+        #region Properties
+        private User _currentUser;
         private List<Chat> _chats;
+        #endregion
+        #region Services
+        private readonly IDataContext _userContext;
         private readonly IAppdataService _appdataService;
-        public ChatService(IAppdataService appdataService)
+        #endregion
+        public ChatService(IDataContext dataContext, IAppdataService appdataService)
         {
+            _userContext = dataContext;
             _appdataService = appdataService;
+            var currentUser = _userContext.GetCurrentData();
+            if (currentUser is not User || currentUser is null)
+                throw new ArgumentException("СurrentUser must be a valid instance of User", nameof(currentUser));
+            _currentUser = currentUser as User;
+            _chats = _currentUser.Chats;
         }
-        public Chat? CreateChat(Chat newChat)
+        #region Methods
+        public List<Chat> GetChats() => _chats;
+        public async Task AddChatAsync(Chat newChat)
         {
-            throw new NotImplementedException();
+            var nextId = _chats.Any() ? _chats.Max(u => u.IdChat) + 1 : 1;
+            newChat.IdChat = nextId;
+            _chats.Add(newChat);
+            await _appdataService.SaveDataAsync();
+        }
+        public async Task EditChatAsync(int idChat, Chat newChat)
+        {
+            Chat? chatToEdit = _chats.FirstOrDefault(x => x.IdChat == idChat);
+            if (chatToEdit is null)
+                throw new InvalidOperationException($"Chat with ID {idChat} not found");
+            chatToEdit.Name = newChat.Name;
+            await _appdataService.SaveDataAsync();
+        }
+        public async Task DeleteChatAsync(int idChat)
+        {
+            if (_chats.Any(u => u.IdChat == idChat))
+            {
+                _chats.RemoveAll(u => u.IdChat == idChat);
+                await _appdataService.SaveDataAsync();
+            }
+            else
+                throw new InvalidOperationException($"Chat with ID {idChat} not found");
         }
 
-        public bool DeleteChat(int idChat)
+        public Chat? GetChatById(int idChat)
         {
-            throw new NotImplementedException();
+            Chat? chat = _chats.FirstOrDefault(x => x.IdChat == idChat);
+            if (chat is null)
+                throw new InvalidOperationException($"User with ID {idChat} not found");
+            return chat;
         }
-
-        public bool EditChat(int idChat, Chat newChat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Chat? GetChat(int idChat)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Chat> GetChats()
-        {
-            return _chats;
-        }
+        #endregion
     }
 }
--- VKTChat/VKTChat.Core/Services/IChatService.cs
+++ VKTChat/VKTChat.Core/Services/IChatService.cs
@@ -4,10 +4,10 @@ namespace VKTChat.Core.Services
 {
     public interface IChatService
     {
-        Chat? CreateChat(Chat newChat);
-        bool EditChat(int idChat, Chat newChat);
-        bool DeleteChat(int idChat);
         List<Chat> GetChats();
-        Chat? GetChat(int idChat);
+        Task AddChatAsync(Chat newChat);
+        Task EditChatAsync(int idChat, Chat newChat);
+        Task DeleteChatAsync(int idChat);
+        Chat? GetChatById(int idChat);
     }
 }
